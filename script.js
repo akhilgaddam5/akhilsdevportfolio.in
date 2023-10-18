aed9dcf4a1e48fe11c012af47cd866c126c08109
akhilgaddam5 <144714253+akhilgaddam5@users.noreply.github.com>
2023-10-18 15:41:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add your JavaScript code here
+console.log("JavaScript is running!");
+
+// Example event listener
+document.querySelector('button').addEventListener('click', () => {
+    alert('Button clicked!');
+});
